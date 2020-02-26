5ba8dd3bf3cb51d81766eadafd4baae3421e5795
nz1012861 <nz68@njit.edu>
2020-02-25 23:00:37 -0500

    project

--- project/home.php
+++ project/home.php
@@ -1,7 +1,7 @@
 <html>
 	<head>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-		<title>Jason Paprocki</title>
+		<title>Nelson Zhou</title>
 	</head>
     <header>
         <div class="nav justify-content-md-center">
